3659ca250df53089f2e78366d56a09db923b8e83
entira <patrik.skovajsa@entira.sk>
2025-06-15 15:18:54 +0200

    cleanup

--- pdf_process_clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Clean PDF processor without warnings
-
-export PYTHONWARNINGS=ignore
-export TF_CPP_MIN_LOG_LEVEL=3
-export TOKENIZERS_PARALLELISM=false
-
-python3 pdf_library_processor.py "$@" 2>/dev/null
